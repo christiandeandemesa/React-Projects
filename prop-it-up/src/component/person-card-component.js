2ea167521d184476b5aa0a17688907ce66512c71
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-10-25 21:47:33 -0700

    putting it together commit

--- prop-it-up/src/component/person-card-component.js
+++ prop-it-up/src/component/person-card-component.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+    bdayButton = () => {
+        this.setState({age: this.state.age + 1})
+    }
     render(){
         const {firstName, lastName, age, hairColor} = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick = {this.bdayButton}>Birthday Button for {firstName} {lastName}</button>
             </div>
         );
     }
